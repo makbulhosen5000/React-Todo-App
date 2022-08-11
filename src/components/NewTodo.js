c687585b6b7e488bf717647f3c78a1ec1f34c46c
mhakash5000 <mhakash5000@gmail.com>
2022-08-12 00:29:40 +0600

    added form by event hanlder

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './newtodo.module.css'
 
 function NewTodo() {
+  const [todo,setTodo] = useState({title:"",desc:""});
+  const {title,desc} = todo;
+
+  const changeHandler = (e) =>{
+   const name = e.target.name;
+   setTodo((oldTodo)=>{
+    return {...oldTodo,[name]:e.target.value}
+   })
+  }
+  const formHandler = (e) =>{
+   e.preventDefault();
+  }
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={formHandler}>
         <div className={style["form-field"]}>
             <label htmlFor='title'>Title</label>
-            <input type="text" id="title" name="title" placeholder="add title" />
+            <input type="text" value={title} id="title" name="title" placeholder="add title" onChange={changeHandler} />
         </div>
         <div className={style["form-field"]}>
             <label htmlFor='description'>description</label>
-            <textarea type="text" id="desc" name="desc" placeholder="add description" />
+            <textarea type="text" value={desc} id="desc" name="desc" placeholder="add description" onChange={changeHandler} />
         </div>
         <button type='submit'>Add Todo</button>
     </form>
